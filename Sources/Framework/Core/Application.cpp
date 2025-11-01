42fad40937772f30278f482b6db327ca7bdc1595
ASOBIBA <86977637+LHBM04@users.noreply.github.com>
2025-11-02 02:28:35 +0900

    Update Application class

--- Sources/Framework/Core/Application.cpp
+++ Sources/Framework/Core/Application.cpp
@@ -1,30 +1,48 @@
 #include "Application.h"
 
-void Application::Initialize()
+bool Application::Initialize(const Application::Specification& specification_)
 {
-    WindowSpecification spec{};
-    spec.x = 100;
-    spec.y = 100;
-    spec.width = 800;
-    spec.height = 600;
-    spec.title = "My Application";
-    spec.resizable = true;
-    spec.fullscreen = false;
-    spec.vsync = true;
-    spec.decorated = true;
+    specification = specification_;
+    {
+        Window::Specification spec{};
+        spec.x          = 100;
+        spec.y          = 100;
+        spec.width      = 800;
+        spec.height     = 600;
+        spec.title      = "My Application";
+        spec.resizable  = true;
+        spec.fullscreen = false;
+        spec.vsync      = true;
+        spec.decorated  = true;
 
-    window = std::make_unique<Window>(spec);
-    graphics = std::make_unique<Graphics>();
+        window = std::make_unique<Window>(spec);
+    }
+    {
+        Graphics::Specification spec{};
+        spec.x      = 0;
+        spec.y      = 0;
+        spec.width  = 800;
+        spec.height = 600;
+
+        graphics = std::make_unique<Graphics>(spec);
+    }
+
+    return true;
 }
 
 void Application::Run()
 {
     while (!window->ShouldClose())
     {
+        window->Update();
+
         graphics->PreRender();
         graphics->PostRender();
     }
 }
 
+Application::Specification Application::specification{};
+
 std::unique_ptr<Window> Application::window = nullptr;
+
 std::unique_ptr<Graphics> Application::graphics = nullptr;
--- Sources/Framework/Core/Application.h
+++ Sources/Framework/Core/Application.h
@@ -1,18 +1,70 @@
 #ifndef GUARD_APPLICATION_H
 #define GUARD_APPLICATION_H
 
-#include <memory>
+#include "../../PCH.h"
 
-#include "Window.h"
 #include "Graphics.h"
+#include "Window.h"
 
+/**
+ * @class Application
+ *
+ * @brief 애플리케이션을 정의합니다.
+ */
 class Application final
 {
 public:
-    static void Initialize();
+    /**
+     * @struct Specification
+     *
+     * @brief 애플리케이션의 사양을 정의합니다.
+     */
+    struct Specification final
+    {
+        /**
+         * @brief 애플리케이션의 이름.
+         */
+        const char* name;
+
+        /**
+         * @brief 애플리케이션의 너비.
+         */
+        int width;
+
+        /**
+         * @brief 애플리케이션의 높이.
+         */
+        int height;
+    };
+
+    /**
+     * @brief 애플리케이션을 초기화합니다.
+     *
+     * @param specification_ 애플리케이션의 사양.
+     *
+     * @return bool 초기화에 성공했다면 true, 아니면 false.
+     */
+    static bool Initialize(const Application::Specification& specification_);
+
+    /**
+     * @brief 애플리케이션을 실행합니다.
+     */
     static void Run();
+
 private:
+    /**
+     * @brief 해당 애플리케이션의 사양.
+     */
+    static Application::Specification specification;
+
+    /**
+     * @brief 해당 애플리케이션의 윈도우.
+     */
     static std::unique_ptr<Window> window;
+
+    /**
+     * @brief 해당 애플리케이션의 그래픽스.
+     */
     static std::unique_ptr<Graphics> graphics;
 };
 
